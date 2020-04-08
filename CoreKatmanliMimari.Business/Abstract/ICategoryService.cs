98e8e26d32c37547936b987eb1c46933b896fc08
alieryaman <postauyelik@gmail.com>
2020-04-09 01:34:13 +0300

    8-Business Katmanının Oluşturulması ve Servislerın Kodlanması

---
+++ CoreKatmanliMimari.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using CoreKatmanliMimari.Entitiy.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreKatmanliMimari.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        Category Add(Category category);
+        Task<Category> AddAsync(Category category);
+        Category Update(Category category);
+        Task<Category> UpdateAsync(Category category);
+        void Delete(Category category);
+        Category GetById(int id);
+        List<Category> GetList();
+    }
+}
