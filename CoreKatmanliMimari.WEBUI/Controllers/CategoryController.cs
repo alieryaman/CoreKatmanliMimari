870725c983905cbeb49fc8bf76f55393fbb79a39
alieryaman <postauyelik@gmail.com>
2020-04-11 01:13:44 +0300

    12. Layout Düzenlemesi ve Category Controller,Model,View Oluşturulması

---
+++ CoreKatmanliMimari.WEBUI/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreKatmanliMimari.Business.Abstract;
+using CoreKatmanliMimari.WEBUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreKatmanliMimari.WEBUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public IActionResult GetCategories()
+        {
+            var categoryViewModel = new CategoryViewModel
+            {
+                Categories = _categoryService.GetList()
+            };
+            return View(categoryViewModel);
+        }
+    }
+
+}
---
+++ CoreKatmanliMimari.WEBUI/Models/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CoreKatmanliMimari.Entitiy.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreKatmanliMimari.WEBUI.Models
+{
+    public class CategoryViewModel
+    {
+
+        public Category Category { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
